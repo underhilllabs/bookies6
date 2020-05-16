87bd91c676d2bac4bd8bbb66c8846dd09fce57de
Bart Lantz <underhilllabs@gmail.com>
2020-05-15 23:04:02 -0600

    updates migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_151930) do
+ActiveRecord::Schema.define(version: 2019_12_29_142613) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "url"
+    t.string "hashed_url"
+    t.integer "user_id", null: false
+    t.text "desc"
+    t.string "title"
+    t.boolean "private"
+    t.boolean "is_archived"
+    t.string "archive_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_08_19_151930) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "users"
 end
