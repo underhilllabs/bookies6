ba8925d199d33869025c3ff67d5af210cc5e31b8
Bart Lantz <underhilllabs@gmail.com>
2020-07-05 07:47:33 -0600

    removes unused method shwo

--- app/controllers/bookmarklet_controller.rb
+++ app/controllers/bookmarklet_controller.rb
@@ -1,30 +1,6 @@
 class BookmarkletController < ApplicationController
   def new
-    @bookmark = Bookmark.new
+    @bookmark = Bookmark.find_or_initialize_by(user: current_user, address: params[:address])
     authorize @bookmark
-  end
-
-  def show
-    @bookmark = Bookmark.find(:id)
-  end
-
-  def create
-    @bookmark = Bookmark.find_or_initialize_by(user_id: params[:bookmark][:user_id], address: params[:bookmark][:address] )
-    respond_to do |format|
-      if @bookmark.update(bookmark_params)
-        format.html { redirect_to show_bookmarklet_path @bookmark, notice: 'Bookmark was successfully created.' }
-        format.json { render :show, status: :created, location: @bookmark }
-      else
-        format.html { render :new }
-        format.json { render json: @bookmark.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  private
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def bookmark_params
-    params.require(:bookmark).permit(:address, :hashed_url, :user_id, :description, :title, :private, :is_archived, :archive_url)
   end
 end
