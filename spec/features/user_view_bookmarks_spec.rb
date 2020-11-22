66495b2927a91afb621482f797f06ce62b0ef0af
Bart Lantz <underhilllabs@gmail.com>
2020-11-21 20:42:01 -0700

    adds feature spec for viewing bookmarks

---
+++ spec/features/user_view_bookmarks_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "the bookmarks page", type: :feature do
+  before :context do
+    User.create(email: 'user@example.com', password: 'password')
+    Bookmark.create(title: 'private bookmark', private: true, address: 'http://private.org', user_id: 1)
+    Bookmark.create(title: 'public bookmark', private: false, address: 'http://bigox.org', user_id: 1)
+  end
+
+  it "shows anonymous user public bookmarks" do
+    visit "/"
+    expect(page).to have_content 'public bookmark'
+  end
+
+  it "does not show anonymous user private bookmarks" do
+    visit "/"
+    expect(page).to_not have_content 'private bookmark'
+  end
+
+  it "shows authenticated user their private bookmarks" do
+    visit "/users/sign_in"
+    within("#new_user") do
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'password'
+    end
+    click_button 'Log in'
+
+    visit "/"
+    expect(page).to have_content 'public bookmark'
+    expect(page).to have_content 'private bookmark'
+  end
+end
